bc3b37d665a384cd264c283b2c19960d5b27d04d
mgz708 <mgz708@corp.auction.co.kr>
2014-10-15 17:56:55 +0900

    > commit

--- examples/jquery-plugin/jquery-imageSlider.navigation.js
+++ examples/jquery-plugin/jquery-imageSlider.navigation.js
@@ -5,7 +5,7 @@
     var len = $(this).find('.slide').find('li').length;
     var $a = '';
     for(var i = 1; i <= len ;i++){
-      $a = $a + '<a href="#page=' + i +'" data-index="' + i + '"><span class="bland">' + i + '</span></a>';
+      $a = $a + '<a href="#page=' + i +'" data-index="' + (i - 1) + '"><span class="bland">' + i + '</span></a>';
     }
     $(this).find('.slide-nav').html($a);
     return this;
