0466b4e47e2537c49d987dad020a31a0b87ed066
mgz708 <mgz708@corp.auction.co.kr>
2014-10-15 16:46:12 +0900

    > module update

--- examples/jquery-plugin/jquery-imageSlider.js
+++ examples/jquery-plugin/jquery-imageSlider.js
@@ -6,12 +6,22 @@
     var defaults = {
       $this : self,
       hover : true,
+      userNav : true,
       rotateTime : 1000,
       slideTime : 3000,
       slideWidth : 200,
       easing : 'slide'
     };
     
+    function move(element){
+      element.imageSliderMove();
+    }
+    
+    function resume(element, options){
+      return setInterval(function(){
+        element.imageSliderRotate(options);
+      }, options.slideTime);
+    }
     return this.each(function(){
       var self = $(this);
       var settings = $.extend(true, defaults, options || {});
@@ -19,23 +29,24 @@
       var intervalTime = null;
       
        $li.each(function(index){
-         $(this).css('left', index * settings.slideWidth);
+         $(this).data('index', index).css('left', index * settings.slideWidth);
       });
       
       if(settings.hover){
         $li.on('mouseover', function(){
           clearInterval(intervalTime);
         }).on('mouseout', function(){
-          intervalTime = setInterval(function(){
-            $li.imageSliderRotate(settings);
-          }, settings.slideTime);
+          intervalTime = resume($li, settings);
         });
       }
       
-      intervalTime = setInterval(function(){
-        $li.imageSliderRotate(settings);
-      }, settings.slideTime);
+      intervalTime = resume($li, settings);
       
+      if(settings.userNav){
+        self.imageSliderNavigation().on('mouseover', function(){
+          move($li);
+        });
+      }
     });
   };
 })(window, jQuery);
