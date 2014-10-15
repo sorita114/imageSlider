c3b069fec53f4d1f1c790ec42834faf1c2d28021
mgz708 <mgz708@corp.auction.co.kr>
2014-10-15 17:54:36 +0900

    > interFace 형식으로 모듈 분리 > 이벤트 생성 및 수정

--- examples/jquery-plugin/jquery-imageSlider.js
+++ examples/jquery-plugin/jquery-imageSlider.js
@@ -2,7 +2,6 @@
   'use strict';
   
   $.fn.imageSlider = function(options){
-    //var self = this;
     var defaults = {
       $this : self,
       hover : true,
@@ -13,40 +12,23 @@
       easing : 'slide'
     };
     
-    function move(element){
-      element.imageSliderMove();
-    }
-    
-    function resume(element, options){
-      return setInterval(function(){
-        element.imageSliderRotate(options);
-      }, options.slideTime);
-    }
     return this.each(function(){
-      var self = $(this);
       var settings = $.extend(true, defaults, options || {});
-      var $li = self.find('.slide').find('li');
-      var intervalTime = null;
       
-       $li.each(function(index){
-         $(this).data('index', index).css('left', index * settings.slideWidth);
+      //rotae
+      var rotate = $(this).find('.slide').imageSliderRotate(settings);
+      //navigtion
+      var nav = $(this).imageSliderNavigation(options);
+      
+      //init
+      rotate.resume();
+      
+      //events
+      nav.on('click','a', function(e){
+        var pageNum = $(this).data('index');
+        e.preventDefault();
+        rotate.moveTo(pageNum);
       });
-      
-      if(settings.hover){
-        $li.on('mouseover', function(){
-          clearInterval(intervalTime);
-        }).on('mouseout', function(){
-          intervalTime = resume($li, settings);
-        });
-      }
-      
-      intervalTime = resume($li, settings);
-      
-      if(settings.userNav){
-        self.imageSliderNavigation().on('mouseover', function(){
-          move($li);
-        });
-      }
     });
   };
 })(window, jQuery);
--- examples/jquery-plugin/jquery-imageSlider.rotate.js
+++ examples/jquery-plugin/jquery-imageSlider.rotate.js
@@ -2,24 +2,57 @@
   'use strict';
   
   $.fn.imageSliderRotate = function(options){
-    var len = $(this).length;
-    $(this).animate({
-      'left' : '-=' + options.slideWidth
-    },{
-      duration : options.rotateTime,
-      step : function(now, tw){
-        if(now === 0){
-          $(this).addClass('active').siblings().removeClass('active');
-        }
-      },
-      complete : function(){
-        var $this = $(this);
-        var left = $this.position().left;
-        if( left  < 0 ){
-          $this.css('left' , (len - 1) * options.slideWidth + 'px');
-        }
-      }
-    }, options.slideTime);
+    var self = $(this);
+    var $li = self.find('li');
+    var len = $li.length;
+    var interValTime = null;
     
+    $li.each(function(index){
+      $(this).css('left', index * options.slideWidth);
+      $(this).data('index', index + 1);
+    });
+    
+    return {
+      rotate : function(){
+        $li.animate({
+          'left' : '-=' + options.slideWidth
+        },{
+          duration : options.rotateTime,
+          step : function(now, tw){
+            if(now === 0){
+              $(this).addClass('active').siblings().removeClass('active');
+            }
+          },
+          complete : function(){
+            var $this = $(this);
+            var left = $this.position().left;
+            if( left  < 0 ){
+              $this.css('left' , (len - 1) * options.slideWidth + 'px');
+            }
+          }
+        });
+      },
+      resume : function(){
+        var _this = this;
+        interValTime = setInterval(function(){
+          _this.rotate();
+        }, options.slideTime);
+      },
+      pause : function(){
+        clearInterval(interValTime);
+      },
+      moveTo : function(pageNum){
+        var _this = this;
+        _this.pause();
+        
+        $li.each(function(){
+          if($(this).data('index') !== pageNum){
+            _this.rotate();
+          } else {
+            return false;
+          }
+        });
+      }
+    };
   };
 })(window, jQuery);
