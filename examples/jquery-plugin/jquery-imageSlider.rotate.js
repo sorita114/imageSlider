de874d58eeda2b2d14a68d0578f40b7d895cc34f
mgz708 <mgz708@corp.auction.co.kr>
2014-10-16 13:38:29 +0900

    > moveTo 구현

--- examples/jquery-plugin/jquery-imageSlider.rotate.js
+++ examples/jquery-plugin/jquery-imageSlider.rotate.js
@@ -7,27 +7,44 @@
     var len = $li.length;
     var interValTime = null;
     
+    //init
     $li.each(function(index){
-      $(this).css('left', index * options.slideWidth);
+      $(this).css({
+        'left': index * options.slideWidth
+      });
       $(this).data('index', index + 1);
     });
     
     return {
-      rotate : function(){
+      rotate : function(page){
+        var _this = this;
+        var index = self.find('li.active').data('index');
         $li.animate({
-          'left' : '-=' + options.slideWidth
+          'left' : ( page && index > page ? '+=' : '-=') + options.slideWidth
         },{
-          duration : options.rotateTime,
+          duration : page ? '400' : options.rotateTime,
           step : function(now, tw){
+            $(this).removeClass('active');
             if(now === 0){
-              $(this).addClass('active').siblings().removeClass('active');
+              $(this).addClass('active');
             }
           },
           complete : function(){
             var $this = $(this);
             var left = $this.position().left;
-            if( left  < 0 ){
-              $this.css('left' , (len - 1) * options.slideWidth + 'px');
+            if( left  <  -1 * options.slideWidth ){
+              $this.css('left' , ((len - 1) * options.slideWidth) + ( -1 * options.slideWidth ) + 'px');
+            }
+            if( left >= (len - 1 ) * options.slideWidth ) {
+              $this.css('left' , -1 * options.slideWidth + 'px');
+            }
+            if( page && $this.hasClass('active')){
+              var _index = $this.data('index');
+              if(_index == page){
+                  return;
+              } else {
+                  _this.rotate(page);
+              }
             }
           }
         });
@@ -44,14 +61,7 @@
       moveTo : function(pageNum){
         var _this = this;
         _this.pause();
-        
-        $li.each(function(){
-          if($(this).data('index') !== pageNum){
-            _this.rotate();
-          } else {
-            return false;
-          }
-        });
+        _this.rotate(pageNum);
       }
     };
   };
